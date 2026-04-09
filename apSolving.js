98ab5490008b6773372b9e6077d514c1e188f703
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2026-04-09 12:03:47 +0530

    chore: modified files

--- apSolving.js
+++ apSolving.js
@@ -40,7 +40,7 @@ const days2 = [
 function longestStreak(days2) {
   let count = 0;
   let maxCount = 0;
-  let lengthOfStreak = 0; 
+  let lengthOfStreak = 0;
   for (let i = 0; i < days2.length; i++) {
     if (days2[i] === "commit") {
       count++;
@@ -50,13 +50,13 @@ function longestStreak(days2) {
       }
       //Math.max(count, maxCount);
     } else {
-        if(count >= 3){
-            lengthOfStreak++; 
-        }
+      if (count >= 3) {
+        lengthOfStreak++;
+      }
       count = 0;
     }
   }
-  return {maxCount, lengthOfStreak};
+  return { maxCount, lengthOfStreak };
 }
 
 console.log(longestStreak(days2), "count");
@@ -70,23 +70,74 @@ const code = [
   "no-activity",
   "commit",
   "commit",
-  "commit", "commit", "commit",
+  "commit",
+  "commit",
+  "commit",
 ];
 
-function longestStreak(code){
-  let count = 0; 
-  let maxCount = 0; 
+function longestStreak(code) {
+  let count = 0;
+  let maxCount = 0;
 
-for(let i = 0; i <= code.length; i++){
-  if(code[i] === "commit"){
-    count++; 
-    if(count > maxCount){
-      maxCount = count; 
+  for (let i = 0; i <= code.length; i++) {
+    if (code[i] === "commit") {
+      count++;
+      if (count > maxCount) {
+        maxCount = count;
+      }
+    } else {
+      count = 0;
     }
-  } else{
-    count = 0; 
   }
-}
-return maxCount; 
+  return maxCount;
 }
 console.log(longestStreak(code), "out");
+
+//7 Apr, 2026 ---  Again starting to do Problem Solving everyday for sure.
+
+// from 9 for super;
+const ddd = ["commit", "commit", "no-activity", "commit", "commit", "commit"];
+
+function longesttStreak(ddd) {
+  let count = 0;
+  let maxCount = 0;
+  for (let i = 0; i < ddd.length; i++) {
+    if (ddd[i] === "commit") {
+      count++;
+
+      if (count > maxCount) {
+        maxCount = count;
+      }
+    } else {
+      count = 0;
+    }
+  }
+  return maxCount;
+}
+
+console.log(longesttStreak(ddd), "hhjhjhhjh");
+
+const ddd2 = ["commit", "commit", "no-activity", "commit", "commit", "commit"];
+
+// function longesttStreak(ddd2) {
+//   let count = 0;
+//   let maxCount = 0;
+//   let lengthtreak = 0; 
+//   for (let i = 0; i < ddd2.length; i++) {
+//     if (ddd2[i] === "commit") {
+//       count++;
+
+//       if (count > maxCount) {
+//         maxCount = count;
+//       }
+//     } else {
+//       if(count >= 3){
+//         lengthtreak++; 
+//       }
+//       count = 0;
+//     }
+//   }
+//   return { maxCount, lengthtreak };
+// }
+
+// console.log(longesttStreak(ddd2), "hhjhjhhjh");
--- basic.js
+++ basic.js
@@ -1,23 +1,20 @@
 let arr = [ 1, 2, 3, 4, 5, 6, 7, 9]; 
-
 // suppose we have to add 2 with each number in the above Array.
 
 let newArr = arr.map((num) => num + 2); 
-// console.log(newArr, "newArr");
-
-// console.log(arr, "arr"); 
-
+console.log(newArr, "newArr");
+console.log(arr, "arr"); 
 
 arr.forEach((num) => {
-    console.log(num + 2); 
+    console.log(num + 2, "forEach running."); 
 }
 ); 
 
 
 
-let anarray = [1, 2, 3, 4, 5, 6]
+// let anarray = [1, 2, 3, 4, 5, 6]
 
-const sumOf2 = anarray.map((num) => num + 2); 
-console.log(sumOf2, "sumOf2"); 
+// const sumOf2 = anarray.map((num) => num + 2); 
+// console.log(sumOf2, "sumOf2"); 
 
 
